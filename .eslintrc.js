ca2dbbbc23b07cc33ca813664905656f74081c2a
mkanenobu <kaneda.1018@gmail.com>
2020-07-25 23:12:35 +0900

    add CI, add lint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.export = {
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint"
+  ],
+  "plugins": ["@typescript-eslint"],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "env": { "node": true, "es6": true },
+}
