4965f538a4b78877656c6b202a7496113ecae544
mkanenobu <kaneda.1018@gmail.com>
2020-07-25 21:18:17 +0900

    add db

--- src/routers/users.routes.ts
+++ src/routers/users.routes.ts
@@ -3,15 +3,15 @@ import { UserController } from "../controllers/users.controller";
 
 export const UserRouter = new Router({ prefix: "/users" });
 
-UserRouter.get("/", (ctx) => {
-  ctx.body = UserController.getAll();
+UserRouter.get("/", async (ctx) => {
+  ctx.body = await UserController.getAll();
 });
 
 UserRouter.get("/:id", (ctx) => {
-  const user = UserController.findById(ctx.params);
-  if (!user) {
-    ctx.status = 404;
-  } else {
-    ctx.body = user;
-  }
+  // const user = UserController.findById(ctx.params);
+  // if (!user) {
+  //   ctx.status = 404;
+  // } else {
+  //   ctx.body = user;
+  // }
 });
